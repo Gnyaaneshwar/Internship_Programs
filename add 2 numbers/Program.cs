a3d518bbf1bfbfc3e4ba49d3973ab6c8890c76c2
Gnyaaneshwar <gnyaanesh0926@gmail.com>
2026-01-14 12:27:40 +0530

    Add files via upload

---
+++ add 2 numbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+class Program
+{
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+    static void Main()
+
+    {
+        Console.WriteLine("--------addding two numbers-----------");
+        int num1, num2;
+        while (true)
+        {
+            Console.WriteLine("enter the value of number 1");
+            if (!int.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("invalid number");
+            }
+            else
+            {
+                break;
+            }
+        }
+        while (true)
+        {
+            Console.WriteLine("enter the value of number 2");
+            if (!int.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("invalid number");
+            }
+            else
+            {
+               break ;
+            }
+        }
+        
+            int result = add(num1, num2);
+            Console.WriteLine("sum =" + result);
+        
+        
+    }
+}
+
+        
+        
+          
+        
+        
+       
+        
+        
---
+++ greatest number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static int FindMax(int[] arr)
+    {
+        int max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        return max;
+
+    }
+
+    static void Main()
+    {
+        int[] numbers = new int[5];
+        Console.WriteLine("\n enter the 5 values");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("\n value " + (i + 1) + ":");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\n entered values");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("numbers[" + i + "] = " + numbers[i]);
+        }
+        int max = FindMax(numbers);
+        Console.WriteLine("\n the maximum value of num  is " + max);
+    }
+
+}
